54fca61c3f89abf397da1acf8d66b7f0b0574136
ONeust <84396331+ONeust@users.noreply.github.com>
2021-07-02 11:13:30 +0400

    Create test_items.py

---
+++ test_items.py
@@ -0,0 +1,3 @@
+#В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
+#Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
+#Тест должен запускаться с параметром language следующей командой: pytest --language=es test_items.py
